9e7c17d3513b7f4e8297d5801045cadc85c30f4b
shienacadiz <shienacadiz@gmail.com>
2022-08-25 12:00:50 +0800

    Added validation to temperature unit

--- src/main/java/com/etica/weather/controller/WeatherController.java
+++ src/main/java/com/etica/weather/controller/WeatherController.java
@@ -4,8 +4,11 @@ import com.etica.weather.model.Coordinates;
 import com.etica.weather.model.TempUnitsEnum;
 import com.etica.weather.model.Weather;
 import com.etica.weather.service.WeatherService;
+import org.apache.commons.lang3.EnumUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,10 +24,15 @@ public class WeatherController {
     private String apiKey;
 
     @GetMapping("/weather/{location}")
-    public Weather getWeather(@PathVariable("location") String location,
-                              @RequestParam(name = "units", defaultValue = "celsius", required = false) String units) {
+    public ResponseEntity getWeather(@PathVariable("location") String location,
+                                     @RequestParam(name = "units", defaultValue = "celsius", required = false) String units) {
+        if(!EnumUtils.isValidEnum(TempUnitsEnum.class, units)) {
+            return ResponseEntity.badRequest()
+                    .body(String.format("Invalid values of temperature unit. Valid values are [%s]",
+                            StringUtils.join(TempUnitsEnum.values(), ", ")));
+        }
         Weather weather = weatherService.getWeather(location, TempUnitsEnum.valueOf(units.toLowerCase()).label, apiKey);
-        return weather;
+        return ResponseEntity.ok().body(weather);
     }
 
     @GetMapping("/geolocation/{location}")
---
+++ src/main/java/com/etica/weather/model/WeatherResponse.java
@@ -0,0 +1,4 @@
+package com.etica.weather.model;
+
+public class WeatherResponse {
+}
